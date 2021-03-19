326f3af0db4ea08d774d297f6662a591f0e35e60
ose20 <ayato96bell@gmail.com>
2021-03-19 19:20:09 +0900

    first commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,129 @@
+;;; Emacs 実践入門から引用
+;;; P61 Elisp配置用のディレクトリを作成
+;; load-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;; 以下はもともと ~/.emacs にあったものをここに移植したもの
+;; Emacs 実践入門p16 において、設定ファイルは ~/.emacs.d/ にまとめる方法
+;; を勧めていたので採用
+
+;; Basic .emacs with a good set of defaults, to be used as template for usage
+;; with OCaml and OPAM
+;;
+;; Author: Louis Gesbert <louis.gesbert@ocamlpro.com>
+;; Released under CC0
+
+;; Generic, recommended configuration options
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-use-fuzzy nil)
+ '(backup-directory-alist (quote (("." . "~/.local/share/emacs/backups"))))
+ '(compilation-context-lines 2)
+ '(compilation-error-screen-columns nil)
+ '(compilation-scroll-output t)
+ '(compilation-search-path (quote (nil "src")))
+ '(custom-safe-themes
+   (quote
+    ("0f0a885f4ce5b6f97e33c7483bfe4515220e9cbd9ab3ca798e0972f665f8ee4d" default)))
+ '(electric-indent-mode nil)
+ '(indent-tabs-mode nil)
+ '(line-move-visual t)
+ '(next-error-highlight t)
+ '(next-error-highlight-no-select t)
+ '(next-line-add-newlines nil)
+ '(require-final-newline t)
+ '(sentence-end-double-space nil)
+ '(show-paren-mode t)
+ '(show-trailing-whitespace t)
+ '(visible-bell t))
+
+;; ANSI color in compilation buffer
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; Some key bindings
+
+(global-set-key [f3] 'next-match)
+(defun prev-match () (interactive nil) (next-match -1))
+(global-set-key [(shift f3)] 'prev-match)
+(global-set-key [backtab] 'auto-complete)
+;; OCaml configuration
+;;  - better error and backtrace matching
+
+(defun set-ocaml-error-regexp ()
+  (set
+   'compilation-error-regexp-alist
+   (list '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
+    2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face)))))
+
+(add-hook 'tuareg-mode-hook 'set-ocaml-error-regexp)
+(add-hook 'caml-mode-hook 'set-ocaml-error-regexp)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b
+;; ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base
+;; ## keep this line
+
+;; init.el を反映させる
+;;(load (expand-file-name "~/.emacs.d/init"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; ~.emacs の移植はここまで
+
+(show-paren-mode t)
+(global-linum-mode t)
+(setq linum-format "%4d ")
+(setq-default indent-tabs-mode nil)
+
+(setq mode-line-position
+      '((-3 "%p")
+        (size-indication-mode (8 " of %I"))
+        (line-number-mode
+         ((column-number-mode (10 " L%l C%c") (6 " L%l")))
+         ((column-number-mode (5 " C%c"))))))
+
+(column-number-mode)
+
+(setq read-file-name-completion-ignore-case nil)
+
+(add-hook 'c++-mode-hook
+          '(lambda ()
+             (c-set-offset 'arglist-intro '++)
+             (c-set-offset 'arglist-cont 0)
+             (c-set-offset 'arglist-close 0)
+             (c-set-offset 'inlambda 0)
+             ))(load-theme 'tango-dark)
+
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
