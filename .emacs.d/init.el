15393a5db46efef49a2f2240ee318c76f6b510fd
ose20 <ayato96bell@gmail.com>
2021-03-20 15:43:48 +0900

    色々追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -76,7 +76,7 @@
  '(next-error-highlight t)
  '(next-error-highlight-no-select t)
  '(next-line-add-newlines nil)
- '(package-selected-packages '(multi-term))
+ '(package-selected-packages '(auto-complete helm-descbinds web-mode helm multi-term))
  '(require-final-newline t)
  '(sentence-end-double-space nil)
  '(show-paren-mode t)
@@ -125,10 +125,78 @@
 
 ;;; ~.emacs の移植はここまで
 
-(show-paren-mode t)
+
+;;; Emacs 実践入門より
+;;; P79-81 お勧めのキー操作
+;; C-mにnewline-and-indentを割り当てる。
+;; 先ほどとは異なりglobal-set-keyを利用
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 折り返しトグルコマンド
+;;(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+
+;; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+
+;; 行番号を常に表示する
+(global-linum-mode t)
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+
+;;; P98 現在行のハイライト
+;(defface my-hl-line-face
+;  ;; 背景がdarkならば背景色を紺に
+;  '((((class color) (background dark))
+;     (:background "NavyBlue" t))
+;    ;; 背景がlightならば背景色を青に
+;    (((class color) (background light))
+;     (:background "LightSkyBlue" t))
+;    (t (:bold t)))
+;  "hl-line's my face")
+;(setq hl-line-face 'my-hl-line-face)
+;(global-hl-line-mode t)
+
+;; P99 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+;;(set-face-background 'show-paren-match-face nil)
+;;(set-face-underline-p 'show-paren-match-face "darkgreen")
+
+;;; P104-105 関数を定義する場合
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+;;; P122-123 候補選択型インタフェース Helm
+(package-install 'helm)
+;; Helm
+(require 'helm-config)
+
+;;; P128 補完入力の強化 Auto Complete Mode
+;;(package-install 'auto-complete)
+;; auto-completeの設定
+(when (require 'auto-complete-config nil t)
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default)
+  (setq ac-use-menu-map t)
+  (setq ac-ignore-case nil))
+
+;;; 最初期に init.el に書いていたもの
 (global-linum-mode t)
 (setq linum-format "%4d ")
-(setq-default indent-tabs-mode nil)
+;;(setq-default indent-tabs-mode nil)
 
 (setq mode-line-position
       '((-3 "%p")
